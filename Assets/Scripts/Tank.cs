0cec2877612ad7c7cdb7d3b796a5a61be084fb20
ilxna <ilvxna@hotmail.com>
2023-07-16 11:06:15 +0800

    add explain:art:

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// tank作为管理水箱的脚步
+/// </summary>
 public class Tank : MonoBehaviour
 {
 	private static Tank _instance;
