e5b7deb58598dcfd78a77ca8ab6904f7693baa09
ilxna <ilvxna@hotmail.com>
2023-07-22 10:10:01 +0800

    add gizmos method

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -24,6 +24,29 @@ public class Tank : MonoBehaviour
 		_tr = transform;
 
 	}
+	private void OnDrawGizmos()
+	{
+		if (this.enabled)
+		{
+			if (_tr == null)
+			{
+				_tr = this.transform;
+			}
+			//记录颜色
+			Color c = Gizmos.color;
+			Gizmos.color = Color.green;
+			//记录matrix
+			Matrix4x4 m = Gizmos.matrix;
+			//赋值为transform的matrix
+			Gizmos.matrix = _tr.localToWorldMatrix;
+			//绘制线框立方体
+			Gizmos.DrawWireCube(Vector3.zero, new Vector3(width, height, depth));
+			//恢复matirx和color
+			Gizmos.matrix = m;
+			Gizmos.color = c;
+		}
+	}
+
 }
 
 
