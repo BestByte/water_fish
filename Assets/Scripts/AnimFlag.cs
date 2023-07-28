fb03344d520753ad42b0966d0b07a95eaacded6b
ilxna <ilvxna@hotmail.com>
2023-07-28 23:06:11 +0800

    :fix: anim flag

--- Assets/Scripts/AnimFlag.cs
+++ Assets/Scripts/AnimFlag.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class ANewBehaviourScript : MonoBehaviour
+[System.Serializable]
+public class AnimFlag
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Transform _tr;
+	private Animator _anim;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private float _speed;
+
+	public AnimFlag(Transform tr)
+	{
+		_tr = tr;
+		_anim = _tr.GetComponent<Animator>();
+	}
+
+	public void SetSpeed(float speed)
+	{
+		_speed = speed;
+
+	}
+	public void Update(float deltaTime)
+	{
+		if (!_anim)
+		{
+			return;
+		}
+		_UpdateAnimSpeed();
+
+	}
+
+	private void _UpdateAnimSpeed()
+	{
+		if (_speed > 1.5f)
+		{
+			_speed = 1.5f;
+		}
+		else if (_speed < 0.2f)
+		{
+			_speed = 0.2f;
+		}
+		_anim.speed = _speed;
+	}
 }
