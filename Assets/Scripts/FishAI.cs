bc2d86daf2c89df3559a1383ef43dd7ffb1f8b8c
ilxna <ilvxna@hotmail.com>
2023-07-29 15:02:40 +0800

    change fish ai

--- Assets/Scripts/FishAI.cs
+++ Assets/Scripts/FishAI.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class FishAI : MonoBehaviour
 {
+	public enum ActionState { Swimming, Float, Escape }
 
 	private Transform _tr;
 	[SerializeField]
@@ -21,32 +24,36 @@ public class FishAI : MonoBehaviour
 
 	[SerializeField]
 	private SpeedFlag speed;
-
 	[SerializeField]
 	private RotateFlag rota;
-	// ...
-
 	[SerializeField]
 	private MoveFlag move;
-	// ...
 	[SerializeField]
 	private AnimFlag anim;
 
 
-	public enum ActionState { Swimming, Float, Escape }
 	public ActionState _curState;
 	public ActionState _lastState;
 
-	private void Swimming()
-	{
-		_lastState = _curState;
-		_curState = ActionState.Swimming;
-		
-    }
 
+	void Start()
+	{
+		_tr = transform;
+		speed = new SpeedFlag(_tr);
+		rota = new RotateFlag(_tr);
+		move = new MoveFlag(_tr);
+		anim = new AnimFlag(_tr);
+		RandomBorn();
+		Swimming();
+	}
+
+	private void RandomBorn()
+	{
+		_tr.position = new Vector3(Tank.instance.RandomX(), Tank.instance.RandomY(), Tank.instance.RandomZ());
+		tarDir = _tr.transform.forward;
+	}
 	void Update()
 	{
-
 		float deltaTime = Time.deltaTime;
 		speed.Update(deltaTime); // Ã¿Ö¡¸üÐÂ
 		rota.Update(deltaTime);
@@ -55,37 +62,10 @@ public class FishAI : MonoBehaviour
 		anim.SetSpeed(speed.curSpeed);
 		anim.Update(deltaTime);
 	}
-
-
-	void Start()
+	private void Swimming()
 	{
-		_tr = transform;
-		speed = new SpeedFlag(_tr);
-		RandomBorn();
-		GetTarget();
-
-		rota = new RotateFlag(_tr);
-
-		
-		// ...
-		move = new MoveFlag(_tr);
-		// ...
-
-		anim = new AnimFlag(_tr);
-	}
-
-	private void RandomBorn()
-	{
-		_tr.position = new Vector3(Tank.instance.RandomX(), Tank.instance.RandomY(), Tank.instance.RandomZ());
-		tarDir = _tr.transform.forward;
-	}
-	private void GetTarget()
-	{
-		move.Move(tarDir, GetTarget);
-
-		float turnSpeed = Random.Range(2f, 5f);
-		rota.SetRotate(turnSpeed, tarDir);
-
+		_lastState = _curState;
+		_curState = ActionState.Swimming;
 
 		float tarX = Tank.instance.RandomX();
 		float tarZ = Tank.instance.RandomZ();
@@ -109,8 +89,110 @@ public class FishAI : MonoBehaviour
 		//        tarSpeed = Random.Range(5f, 10f);
 		speed.SetVarMinSpeed(Random.Range(0f, 2f));
 		speed.SetVarSpeedConfig(Random.Range(0.1f, 0.5f), Random.Range(0.2f, 0.5f));
-		speed.StartVarSpeed(tarDistance, tarTime, GetTarget);
+		speed.StartVarSpeed(tarDistance, tarTime, StartAction);
+
+		float turnSpeed = Random.Range(2f, 5f);
+		rota.SetRotate(turnSpeed, tarDir);
+
+		move.Move(tarDir, Swimming);
+
 		curTime = 0f;
 	}
-	
+
+	// Æ¯¸¡
+	private void Float()
+	{
+		_lastState = _curState;
+		_curState = ActionState.Float;
+		float turnSpeed = Random.Range(2f, 5f);
+		float dis = Random.Range(1, 2);
+		tarDir.y = 0;
+		Vector3 tarPos = _tr.position + tarDir * dis;
+		float tarDistance = 0;
+		if (Tank.instance.InTank(tarPos))
+		{
+			tarDistance = dis;
+		}
+		else
+		{
+			tarPos = Tank.instance.InTankPos(tarPos);
+			tarDistance = Vector3.Distance(_tr.position, tarPos);
+		}
+		speed.StartToTarSpeed(Random.Range(-1f, 1f), Random.Range(0.5f, 2f));
+		rota.SetRotate(turnSpeed, tarDir);
+		move.MoveByTime(Random.Range(2f, 5f), tarDir, StartAction);
+	}
+	private void StartAction()
+	{
+		if (Random.Range(0, 10) > 3)
+		{
+
+			Swimming();
+		}
+		else
+		{
+			Float();
+		}
+	}
+	public void Escape()
+	{
+		_lastState = _curState;
+		_curState = ActionState.Escape;
+
+		if (Random.Range(0, 5) > 2)
+		{
+			Escape_SpeedUp();
+		}
+		else
+		{
+			Escape_Turn();
+		}
+	}
+	private void Escape_SpeedUp()
+	{
+		float dis = Random.Range(10f, 20f);
+		tarPos = _tr.position + tarDir * dis;
+		Tank.instance.InTankPos(tarPos);
+		tarDir = tarPos - _tr.position;
+		tarDir.Normalize();
+		float tarDistance = (tarPos - _tr.position).magnitude;
+
+		float turnSpeed = Random.Range(4f, 10f);
+		tarTime = Random.Range(1f, 3f);
+		tarDir.Normalize();
+		speed.SetVarMinSpeed(Random.Range(0f, 2f));
+		speed.SetVarSpeedConfig(Random.Range(0.02f, 0.1f), Random.Range(0.2f, 0.5f));
+		speed.StartVarSpeed(tarDistance, tarTime, StartAction);
+		rota.SetRotate(turnSpeed, tarDir);
+		move.Move(tarDir, StartAction);
+	}
+	private void Escape_Turn()
+	{
+		float turnSpeed = Random.Range(4f, 10f);
+
+		float tarX = Tank.instance.RandomX();
+		float tarZ = Tank.instance.RandomZ();
+		float angle = Random.Range(-35, 35);
+		float tanAngle = Mathf.Tan(angle * Mathf.Deg2Rad);
+		float tarY = tarX * tanAngle;
+		if (tarY < Tank.instance.minHeightPos)
+		{
+			tarY = Tank.instance.minHeightPos;
+		}
+		else if (tarY > Tank.instance.maxHeightPos)
+		{
+			tarY = Tank.instance.maxHeightPos;
+		}
+
+		tarTime = Random.Range(3f, 5f);
+		tarPos = new Vector3(tarX, tarY, tarZ);
+		tarDir = tarPos - _tr.position;
+		float tarDistance = tarDir.magnitude;
+		tarDir.Normalize();
+		speed.SetVarMinSpeed(Random.Range(0f, 2f));
+		speed.SetVarSpeedConfig(Random.Range(0.02f, 0.1f), Random.Range(0.2f, 0.5f));
+		speed.StartVarSpeed(tarDistance, tarTime, StartAction);
+		rota.SetRotate(turnSpeed, tarDir);
+		move.Move(tarDir);
+	}
 }
