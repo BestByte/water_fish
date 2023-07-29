0d8e7f6e5e78fc1b8ce7d7d0ee8f84bb273ed956
ilxna <ilvxna@hotmail.com>
2023-07-29 15:22:56 +0800

    add enent manager

--- Assets/Scripts/EventMgr.cs
+++ Assets/Scripts/EventMgr.cs
@@ -4,7 +4,55 @@ using UnityEngine;
 
 using System.Collections;
 using System.Collections.Generic;
-public class EventMgr
-{
-}
+using System.Collections;
+using System.Collections.Generic;
+using System; //Action所在的命名空间
 
+public class EventMgr<T> where T : struct
+{
+	private static EventMgr<T> _instance;
+	public static EventMgr<T> instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = new EventMgr<T>();
+			}
+			return _instance;
+		}
+	}
+
+	private Dictionary<T, Action<object[]>> eventDict;
+	public EventMgr()
+	{
+		eventDict = new Dictionary<T, Action<object[]>>();
+	}
+	public void AddListener(T eventName, Action<object[]> cb)
+	{
+		if (eventDict.ContainsKey(eventName))
+		{
+			eventDict[eventName] += cb;
+		}
+		else
+		{
+			eventDict[eventName] = cb;
+		}
+
+	}
+	public void RemListener(T eventName, Action<object[]> cb)
+	{
+		if (eventDict.ContainsKey(eventName))
+		{
+			eventDict[eventName] -= cb;
+		}
+
+	}
+	public void TriggerEvent(T eventName, params object[] param)
+	{
+		if (eventDict.ContainsKey(eventName))
+		{
+			eventDict[eventName](param);
+		}
+	}
+}
