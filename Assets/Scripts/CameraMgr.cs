072206e3970ce4afa934a0da69bc762fdd38c879
ilxna <ilvxna@hotmail.com>
2023-07-29 15:21:29 +0800

    add

--- Assets/Scripts/CameraMgr.cs
+++ Assets/Scripts/CameraMgr.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class CameraMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public static CameraMgr instance;
+
+	public Camera cam;
+	protected void Awake()
+	{
+		instance = this;
+		cam = this.GetComponent<Camera>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Ray r = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitInfo;
+			if (Physics.Raycast(r, out hitInfo, 100f))
+			{
+				FishAI ai = hitInfo.transform.GetComponent<FishAI>();
+				ai.Escape();
+			}
+		}
+	}
 }
