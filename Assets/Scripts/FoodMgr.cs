1be59a7ba756d7c14f3efc34eec5b4fb09cbc822
ilxna <ilvxna@hotmail.com>
2023-07-29 18:26:27 +0800

    Food manager

--- Assets/Scripts/FoodMgr.cs
+++ Assets/Scripts/FoodMgr.cs
@@ -6,6 +6,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class FoodMgr : MonoBehaviour
 {
 	private static FoodMgr _instance;
@@ -36,12 +40,43 @@ public class FoodMgr : MonoBehaviour
 		}
 	}
 
+
 	public GameObject foodGo;
 	public float downSpeed = 5f;
+	public List<FoodAI> foodList;
 
 	public void GenFood(Vector3 pos)
 	{
+		if (foodList == null)
+		{
+			foodList = new List<FoodAI>();
+		}
+
 		GameObject go = GameObject.Instantiate(foodGo);
 		go.transform.position = pos;
+		foodList.Add(go.GetComponent<FoodAI>());
+		EventMgr<FishAI.FishEvent>.instance.TriggerEvent(FishAI.FishEvent.Feed);
+	}
+	public void DesFood(FoodAI ai)
+	{
+		foodList.Remove(ai);
+	}
+	public void Nearest(Vector3 pos, out FoodAI ai, out float dis)
+	{
+		dis = 9999f;
+		ai = null;
+		for (int i = 0, imax = foodList.Count; i < imax; ++i)
+		{
+			FoodAI f = foodList[i];
+			if (f != null && f.active)
+			{
+				float d = Vector3.Distance(pos, f.transform.position);
+				if (d < dis)
+				{
+					ai = f;
+					dis = d;
+				}
+			}
+		}
 	}
 }
