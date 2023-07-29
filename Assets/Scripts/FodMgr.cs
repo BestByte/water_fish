c144c32f39586c5c3e38ecfbba351d6c86383355
ilxna <ilvxna@hotmail.com>
2023-07-29 17:10:45 +0800

    add food manager

--- Assets/Scripts/FodMgr.cs
+++ Assets/Scripts/FodMgr.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class FoodMgr : MonoBehaviour
+{
+	private static FoodMgr _instance;
+	public static FoodMgr instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				GameObject go = new GameObject("FoodMgr");
+				_instance = go.AddComponent<FoodMgr>();
+				DontDestroyOnLoad(go);
+			}
+			return _instance;
+		}
+	}
+	private void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+			this.gameObject.name = "FoodMgr";
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else if (_instance != this)
+		{
+			DestroyImmediate(this, true);
+		}
+	}
+
+	public GameObject foodGo;
+	public float downSpeed = 5f;
+
+	public void GenFood(Vector3 pos)
+	{
+		GameObject go = GameObject.Instantiate(foodGo);
+		go.transform.position = pos;
+	}
 }
