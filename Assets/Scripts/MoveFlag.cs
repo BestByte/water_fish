e84a70d6946ca65e876de44b1106042034ad7e48
ilxna <ilvxna@hotmail.com>
2023-07-29 14:56:50 +0800

    add move flag

--- Assets/Scripts/MoveFlag.cs
+++ Assets/Scripts/MoveFlag.cs
@@ -7,6 +7,11 @@ using System;
 /// <summary>
 /// 移动相关的控制
 /// </summary>
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
 [System.Serializable]
 public class MoveFlag
 {
@@ -19,6 +24,10 @@ public class MoveFlag
 	private Vector3 _dir;
 	[SerializeField]
 	private bool _moving = false;
+	[SerializeField]
+	private bool _movingByTime = false;
+	[SerializeField]
+	private float _moveTime = 0f;
 
 	private Action _callback;
 	public bool moving
@@ -51,8 +60,21 @@ public class MoveFlag
 	public void Move(Vector3 dir, Action cb = null)
 	{
 		_moving = true;
+		_movingByTime = false;
 		_dir = dir;
 		_callback = cb;
+	}
+	/// <summary>
+	/// 如果移动位置超出鱼缸，会callback
+	/// </summary>
+	public void MoveByTime(float time, Vector3 dir, Action cb = null)
+	{
+		_movingByTime = true;
+		_moving = false;
+		_dir = dir;
+		_moveTime = time;
+		_callback = cb;
+
 	}
 	public void Update(float deltaTime)
 	{
@@ -68,6 +90,31 @@ public class MoveFlag
 			{
 				_nowPos = pos;
 			}
+		}
+		else if (_movingByTime)
+		{
+
+			_MoveByTime(deltaTime);
+		}
+	}
+	private void _MoveByTime(float deltaTime)
+	{
+		_moveTime -= deltaTime;
+		if (_moveTime <= 0)
+		{
+			_movingByTime = false;
+			_DoCallBack();
+			return;
+		}
+		Vector3 pos = _nowPos + deltaTime * _dir * _speed;
+		if (!Tank.instance.InTank(pos))
+		{
+			_movingByTime = false;
+			_DoCallBack();
+		}
+		else
+		{
+			_nowPos = pos;
 		}
 	}
 	private void _DoCallBack()
