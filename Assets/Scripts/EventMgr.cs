b792d39f96b1f21de7f4802e9e87419df3bb3445
ilxna <ilvxna@hotmail.com>
2023-07-30 18:54:39 +0800

    up

--- Assets/Scripts/EventMgr.cs
+++ Assets/Scripts/EventMgr.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections;
-using System.Collections.Generic;
 using System; //Action所在的命名空间
 
+/// <summary>
+/// 在需要的地方 添加监听事件
+/// 在不需要监听的地方，移除监听事件
+/// 在事件发生的地方，调用触发事件
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class EventMgr<T> where T : struct
 {
+	/// <summary>
+	/// 单例模式
+	/// </summary>
 	private static EventMgr<T> _instance;
 	public static EventMgr<T> instance
 	{
@@ -22,7 +26,9 @@ public class EventMgr<T> where T : struct
 			return _instance;
 		}
 	}
-
+	/// <summary>
+	/// 直接使用无返回值类型的委托action
+	/// </summary>
 	private Dictionary<T, Action<object[]>> eventDict;
 	public EventMgr()
 	{
