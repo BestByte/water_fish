c00f34ea86f05757c8e751803409defc9f154f40
ilxna <ilvxna@hotmail.com>
2023-07-30 22:59:00 +0800

    add audio manager

--- Assets/Scripts/buyu/AudioManager.cs
+++ Assets/Scripts/buyu/AudioManager.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private static AudioManager _instance;
+	public static AudioManager Instance
+	{
+		get
+		{
+			return _instance;
+		}
+	}
+	private bool isMute = false;
+	public bool IsMute
+	{
+		get
+		{
+			return isMute;
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public AudioSource bgmAudioSource;
+	public AudioClip seaWaveClip;
+	public AudioClip goldClip;
+	public AudioClip rewardClip;
+	public AudioClip fireClip;
+	public AudioClip changeClip;
+	public AudioClip lvUpClip;
+
+	void Awake()
+	{
+		_instance = this;
+		isMute = (PlayerPrefs.GetInt("mute", 0) == 0) ? false : true;
+		DoMute();
+	}
+
+	public void SwitchMuteState(bool isOn)
+	{
+		isMute = !isOn;
+		DoMute();
+	}
+
+	void DoMute()
+	{
+		if (isMute)
+		{
+			bgmAudioSource.Pause();
+		}
+		else
+		{
+			bgmAudioSource.Play();
+		}
+	}
+
+	public void PlayEffectSound(AudioClip clip)
+	{
+		if (!isMute)
+		{
+			AudioSource.PlayClipAtPoint(clip, new Vector3(0, 0, -5));
+		}
+	}
 }
