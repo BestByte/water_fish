67509a4755c26efc6789bedef3e3fcd377e7f331
ilxna <ilvxna@hotmail.com>
2023-07-30 18:45:37 +0800

    up

--- Assets/Scripts/AnimFlag.cs
+++ Assets/Scripts/AnimFlag.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+/// <summary>
+/// ¶¯»­¿ØÖÆÆ÷
+/// </summary>
 [System.Serializable]
 public class AnimFlag
 {
